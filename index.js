b0ccae40ce214497cfbe222bf3cb416c645a19e0
Steffen Olson <sknoslo@gmail.com>
2015-12-10 16:33:02 -0700

    Refactor to better allow custom input/output paths

--- index.js
+++ index.js
@@ -7,37 +7,56 @@ var Elixir = require('laravel-elixir');
 
 var config = Elixir.config;
 
-Elixir.extend('svgstore', function(srcDir, output) {
+Elixir.extend('svgstore', function(baseDir, output) {
   config.svg = {
-    folder: 'svg' || srcDir,
+    folder: 'svg',
     outputFolder: 'svg'
   };
 
-  var output = output || 'store.svg';
+  var paths = prepGulpPaths('**/*.svg', baseDir, output);
 
   new Elixir.Task('svgstore', function () {
+    this.log(paths.src, paths.output);
 
-    gulp.task('svgstore', function() {
-      return gulp
-        .src(config.get('assets.svg.folder') + "/**/*.svg")
-        .pipe(svgmin(function (file) {
-          var prefix = path.basename(file.relative, path.extname(file.relative));
+    return gulp
+      .src(paths.src.path)
+      .pipe(svgmin(function (file) {
+        var prefix = path.basename(file.relative, path.extname(file.relative));
 
-          return {
-            plugins: [{
-              cleanupIDs: {
-                prefix: prefix + '-',
-                minify: true
-              }
-            }]
-          }
-        }))
-        .pipe(svgstore())
-        .pipe(rename(output))
-        .pipe(gulp.dest(config.get('public.svg.outputFolder')))
-        .pipe(new Elixir.Notification('SVGs Compiled!'));
+        return {
+          plugins: [{
+            cleanupIDs: {
+              prefix: prefix + '-',
+              minify: true
+            }
+          }]
+        }
+      }))
+      .pipe(svgstore())
+      .on('error', function (e) {
+        new Elixir.Notification().error(e, 'SVG Compilation Failed');
 
-    });
+        this.emit('end');
+      })
+      .pipe(rename(paths.output.name))
+      .pipe(gulp.dest(paths.output.baseDir))
+      .pipe(new Elixir.Notification('SVGs Compiled!'));
+
   })
-  .watch(config.get('assets.svg.folder') + '/**/*.svg');
+  .watch(paths.src.path)
+  .ignore(paths.output.path);
 });
+
+/**
+ * Prep the Gulp src and output paths.
+ *
+ * @param  {string|array} src
+ * @param  {string|null} baseDir
+ * @param  {string|null}  output
+ * @return {object}
+ */
+var prepGulpPaths = function(src, baseDir, output) {
+  return new Elixir.GulpPaths()
+    .src(src, baseDir || config.get('assets.svg.folder'))
+    .output(output || config.get('public.svg.outputFolder'), 'sprites.svg');
+};
